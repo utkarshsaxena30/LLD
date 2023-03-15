bbe79aaaf33ea1da472da42ea5a3053cc7ee8d09
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-03-15 14:12:46 +0530

    Indentation

--- strategy-design-pattern.cpp
+++ strategy-design-pattern.cpp
@@ -4,46 +4,46 @@
 using namespace std;
 
 class DriveStrategy {
-    public: 
-        virtual void drive() = 0;
-        virtual ~DriveStrategy() = default;
+public: 
+    virtual void drive() = 0;
+    virtual ~DriveStrategy() = default;
 };
 
 class NormalDrive: public DriveStrategy {
-    public: 
-        void drive() override {
-            cout << "Normal Drive Performed" << endl;
-        }
+public: 
+    void drive() override {
+        cout << "Normal Drive Performed" << endl;
+    }
 };
 
 class SportsDrive: public DriveStrategy {
-    public: 
-        void drive() override {
-            cout << "Sports Drive Performed" << endl;
-        }
+public: 
+    void drive() override {
+        cout << "Sports Drive Performed" << endl;
+    }
 };
 
 class Vehicle {
     unique_ptr<DriveStrategy> driveStrategy_;
     
-    public: 
-        Vehicle(unique_ptr<DriveStrategy> driveStrategy) {
-            driveStrategy_ = move(driveStrategy);
-        }
-    
-        void drive() {
-            driveStrategy_->drive();
-        }
+public: 
+    Vehicle(unique_ptr<DriveStrategy> driveStrategy) {
+        driveStrategy_ = move(driveStrategy);
+    }
+
+    void drive() {
+        driveStrategy_->drive();
+    }
 };
 
 class NormalVehicle: public Vehicle {
-    public: 
-        NormalVehicle(): Vehicle(make_unique<NormalDrive>()) {}
+public: 
+    NormalVehicle(): Vehicle(make_unique<NormalDrive>()) {}
 };
 
 class SportsVehicle: public Vehicle {
-    public: 
-        SportsVehicle(): Vehicle(make_unique<SportsDrive>()) {}
+public: 
+    SportsVehicle(): Vehicle(make_unique<SportsDrive>()) {}
 };
 
 int main() {
