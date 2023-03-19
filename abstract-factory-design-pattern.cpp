a9bfe85f7a8c4f03a398a547425072e5f5cd24e0
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-03-19 08:34:06 +0530

    Add example for Abstract design pattern

---
+++ abstract-factory-design-pattern.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Button {
+public:
+    virtual void paint() = 0;
+    virtual ~Button() = default;
+};
+
+class LinuxButton : public Button {
+public:
+    void paint() override {
+        cout << "Painting a Linux button." << endl;
+    }
+};
+
+class WindowsButton : public Button {
+public:
+    void paint() override {
+        cout << "Painting a Windows button." << endl;
+    }
+};
+
+class Checkbox {
+public:
+    virtual void paint() = 0;
+    virtual ~Checkbox() = default;
+};
+
+class LinuxCheckbox : public Checkbox {
+public:
+    void paint() override {
+        cout << "Painting a Linux checkbox." << endl;
+    }
+};
+
+class WindowsCheckbox : public Checkbox {
+public:
+    void paint() override {
+        cout << "Painting a Windows checkbox." << endl;
+    }
+};
+
+class GUIFactory {
+public:
+    virtual Button* createButton() = 0;
+    virtual Checkbox* createCheckbox() = 0;
+    virtual ~GUIFactory() = default;
+};
+
+class LinuxFactory : public GUIFactory {
+public:
+    Button* createButton() override {
+        return new LinuxButton();
+    }
+
+    Checkbox* createCheckbox() override {
+        return new LinuxCheckbox();
+    }
+};
+
+class WindowsFactory : public GUIFactory {
+public:
+    Button* createButton() override {
+        return new WindowsButton();
+    }
+
+    Checkbox* createCheckbox() override {
+        return new WindowsCheckbox();
+    }
+};
+
+int main() {
+    GUIFactory* factory = nullptr;
+    Button* button = nullptr;
+    Checkbox* checkbox = nullptr;
+
+    // create a Linux factory and a Linux button and checkbox
+    factory = new LinuxFactory();
+    button = factory->createButton();
+    checkbox = factory->createCheckbox();
+    button->paint();
+    checkbox->paint();
+    delete button;
+    delete checkbox;
+    delete factory;
+
+    // create a Windows factory and a Windows button and checkbox
+    factory = new WindowsFactory();
+    button = factory->createButton();
+    checkbox = factory->createCheckbox();
+    button->paint();
+    checkbox->paint();
+    delete button;
+    delete checkbox;
+    delete factory;
+
+    return 0;
+}
