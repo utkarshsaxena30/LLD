5bd528bb9687dea115cdce10fe7968ea26f95879
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-03-30 09:32:44 +0530

    Update implementation for strategy design pattern

--- behavioral-design-patterns/strategy.cpp
+++ behavioral-design-patterns/strategy.cpp
@@ -1,55 +1,99 @@
 #include<iostream>
+#include<list>
 #include<memory>
+#include<stdexcept>
 
-using namespace std;
-
-class DriveStrategy {
-public: 
-    virtual void drive() = 0;
-    virtual ~DriveStrategy() = default;
+class DiscountStrategy {
+    public: 
+        virtual int getDiscountAmount(int totalCost) const = 0;
+        virtual ~DiscountStrategy() = default;
 };
 
-class NormalDrive: public DriveStrategy {
-public: 
-    void drive() override {
-        cout << "Normal Drive Performed" << endl;
-    }
+class LoyaltyProgram: public DiscountStrategy {
+    public: 
+        int getDiscountAmount(int totalCost) const override {
+            // Calculate discounted amount based on years of relationship etc.
+            return 100;
+        }
 };
 
-class SportsDrive: public DriveStrategy {
-public: 
-    void drive() override {
-        cout << "Sports Drive Performed" << endl;
-    }
+class PromotionalProgram: public DiscountStrategy {
+    public: 
+        int getDiscountAmount(int totalCost) const override {
+            // Calculate discounted amount based on the promotion used
+            return 50;
+        }
 };
 
-class Vehicle {
-    unique_ptr<DriveStrategy> driveStrategy_;
-    
-public: 
-    Vehicle(unique_ptr<DriveStrategy> driveStrategy) {
-        driveStrategy_ = move(driveStrategy);
-    }
+/****************************************************************************************************/
 
-    void drive() {
-        driveStrategy_->drive();
-    }
+class Product {
+    public:     
+        virtual int getPrice() const = 0;
+        virtual ~Product() = default;
 };
 
-class NormalVehicle: public Vehicle {
-public: 
-    NormalVehicle(): Vehicle(make_unique<NormalDrive>()) {}
+class Phone: public Product {
+    private: 
+        int price_;
+    public:     
+        Phone(int price): price_(price) {}
+
+        int getPrice() const override {
+            return price_;
+        }
 };
 
-class SportsVehicle: public Vehicle {
-public: 
-    SportsVehicle(): Vehicle(make_unique<SportsDrive>()) {}
+class Blanket: public Product {
+    private: 
+        int price_;
+    public: 
+        Blanket(int price): price_(price) {}
+
+        int getPrice() const override {
+            return price_;
+        }
 };
+
+/****************************************************************************************************/
+
+class Cart {
+    private: 
+        std::list<std::shared_ptr<Product>> items_;
+        std::unique_ptr<DiscountStrategy> discountStrategy_;
+    public: 
+        Cart(std::unique_ptr<DiscountStrategy> discountStrategy): discountStrategy_(std::move(discountStrategy)) {}
+
+        void addItem(std::shared_ptr<Product> item) {
+            if(!item) {
+                throw std::invalid_argument("Invalid Product");
+            }
+
+            items_.push_back(item);
+        }
+
+        void removeItem(std::shared_ptr<Product> item) {
+            if(!item) {
+                throw std::invalid_argument("Invalid Product");
+            }
+
+            items_.remove(item);
+        }
+
+        int getCost() const {
+            int totalCost = 0;
+            for(const auto &item: items_) totalCost += item->getPrice();
+            return totalCost - discountStrategy_->getDiscountAmount(totalCost);
+        }
+};
+
+/****************************************************************************************************/
 
 int main() {
-    NormalVehicle normalCar;
-    normalCar.drive();
-    
-    SportsVehicle sportsCar;
-    sportsCar.drive();
-};
+    Cart cart(std::make_unique<PromotionalProgram>());
+
+    cart.addItem(std::make_unique<Phone>(500));
+    cart.addItem(std::make_unique<Blanket>(250));
+
+    std::cout << "Total Cost >> " << cart.getCost() << std::endl;
+}
